286c583762909158a1e21e1c9199e73fde16626c
massive2351 <massive2351@gmail.com>
2021-04-04 05:34:22 +0000

    finsh 検索機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,16 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = User.find(current_user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
